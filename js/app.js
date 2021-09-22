7dc41405a39854c3361fe6d64568b85816c7168f
Danlois Tovar <danlois.tovar@gmail.com>
2021-09-22 14:31:58 -0300

    Se agrega app.js y se empeizan a configurar los selectores.

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// selectores:
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+const paginacion = document.querySelector('#paginacion');
